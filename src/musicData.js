b30af30fa1b2fb8dc44b1738105e397456c694b0
suadpllana <suadpllana14@gmail.com>
2024-10-02 00:36:55 +0200

    first commit

--- src/musicData.js
+++ src/musicData.js
@@ -1,15 +1,11 @@
-import bejtaImg from "./images/bejta.png"
-import bejtaAudio from "./audios/BEJTA - Lule (prod. by BEJTA).mp3";
+
 import rinesanceImg from "./images/dehuNmu.png"
 import rinesanceAudio from "./audios/Rinesancë - Dehu n'mu.mp3";
+
 import ekoImg from "./images/rrethin.png"
 import ekoAudio from "./audios/EKO-RRETHIN-E-NJEJT-Prod-Pluto.mp3"
-import skerdiImg from "./images/cartel.png"
-import skerdiAudio from "./audios/SKERDI x SINGULLAR - CARTEL.mp3";
-import kurMshehImg from "./images/kurMsheh.png"
-import kurMshehAudio from "./audios/Kur Msheh.mp3";
-import nuanceImg from "./images/nuanca.png"
-import nuanceAudio from "./audios/bletapertace ft EREZA - nuance.mp3";
+
+
 
 import bvqhImg from "./images/rinesance.png"
 import bvqhAudio from "./audios/Rinesancë - BVQH (4K LOOPED VIDEO).mp3";
@@ -17,36 +13,16 @@ import bvqhAudio from "./audios/Rinesancë - BVQH (4K LOOPED VIDEO).mp3";
 import influence from "./images/eko.png"
 import influencea from "./audios/EKO-INFLUENCE-E-KEQE.mp3";
 
-import vampirat from "./images/vampirat.png"
-import vampiratA from "./audios/Mal Maliqi - ERDHËN VAMPIRAT (Official Audio).mp3"
 
 import nokia from "./images/nokia.png"
 import nokiaA from "./audios/Buta-Nokia (The Lounge EP).mp3"
 
-import esmeralda from "./images/esmeralda.png"
-import esmeraldaA from "./audios/Amar - Esmeralda (SSF MIXTAPE).mp3"
-
-import pare from "./images/ago.png"
-import pareA from "./audios/Ago - Pare ft. Singi.mp3"
-
-import drake from "./images/drake.png"
-import drakeA from "./audios/DRAKE - FAMILY MATTERS.mp3";
 
 
-import euphoria from "./images/euphoria.png";
-import euphoriaA from "./audios/euphoria.mp3";
 
-import juice from "./images/juice.png"
-import juiceA from "./audios/Juice+WRLD+-+All+Girls+Are+The+Same+(Directed+by+Cole+Bennett).mp3";
 
-import fuck from "./images/fuckLove.png";
-import fuckA from "./audios/XXXTENTACION - Fuck Love (Audio) (feat. Trippie Redd).mp3";
 
-import whore from "./images/whore.png";
-import whoreA from "./audios/jvla+-+such+a+whole+speed+up.mp3";
 
-import ransom from "./images/ransom.png";
-import ransomA from "./audios/Lil+Tecca+-+Ransom+(Official+Music+Video).mp3";
 
 import front from "./images/frontBottoms.png";
 import frontA from "./audios/twin size mattress - the front bottoms lyrics.mp3";
@@ -66,11 +42,44 @@ import cupidA from "./audios/Vietsub Gym Class Heroes - Cupids Chokehold ft. Pat
 import machine from "./images/machine.png";
 import machineA from "./audios/Mitski - Washing Machine Heart (TikTok Remix) [Lyrics].mp3";
 
-import scars from "./images/scars.png";
-import scarsA from "./audios/scars - Novulent [Official Music Video].mp3";
+
 
 import rager from "./images/rager.png";
 import ragerA from "./audios/Mr Rager - Kid Cudi (Lyrics).mp3";
+
+import abyss from "./images/abyss.png"
+import abyssA from "./audios/Abyss.mp3"
+
+import alien from "./images/alien.png"
+import alienA from "./audios/Vundabar - Alien Blues.mp3"
+
+import better from "./images/better.png"
+import betterA from "./audios/Feel Better - Penelope Scott  Lyrics.mp3"
+
+import blondie from "./images/blondie.png"
+import blondieA from "./audios/Blondie.mp3"
+
+import breakit from "./images/break.png"
+import breakitA from "./audios/pinkpantheress - break it off ( slowed  reverb ).mp3"
+
+import illusion from "./images/illusion.png"
+import illusionA from "./audios/none of it was real, it was an illusion... (vague003 - drowning).mp3"
+
+import milk from "./images/milk.png"
+import milkA from "./audios/Milk and Honey.mp3"
+
+import pastlives from "./images/pastlives.png"
+import pastlivesA from "./audios/sapientdream - Pastlives (lyrics).mp3"
+
+import poison from "./images/poison.png"
+import poisonA from "./audios/Grouper - Poison tree (Lyrics).mp3"
+
+
+import surprises from "./images/surprises.png"
+import surprisesA from "./audios/Radiohead - No Surprises (Lyrics).mp3"
+
+import takecare from "./images/takecare.png"
+import takecareA from "./audios/Take Care - Think Of Me Once In A While, Take Care.mp3"
 
 
 
@@ -78,13 +87,69 @@ export const songsData = [
 
     {
         id: 0,
-        imageSrc: bejtaImg,
-        singer: "Bejta",
-        songName: "Lule",
-        audio: bejtaAudio
+        imageSrc: abyss,
+        singer: "Rinesance",
+        songName: "Abyss",
+        audio: abyssA
     },
+
     {
         id: 1,
+        imageSrc: takecare,
+        singer: "Take Care",
+        songName: "Think of me once in a while",
+        audio: takecareA
+    },
+
+    {
+        id: 2,
+        imageSrc: laufrey,
+        singer: "@laufrey",
+        songName: "From the start",
+        audio: laufreyA
+    },
+
+    {
+        id: 3,
+        imageSrc: pastlives,
+        singer: "sapientdream",
+        songName: "Pastlives",
+        audio: pastlivesA
+    },
+
+    {
+        id: 4,
+        imageSrc: influence,
+        singer: "Eko",
+        songName: "Influence e keqe",
+        audio: influencea
+    },
+    {
+        id: 5,
+        imageSrc: machine,
+        singer: "Mitski",
+        songName: "Washing Machine",
+        audio: machineA
+    },
+
+    {
+        id: 6,
+        imageSrc: ecstacy,
+        singer: "SUICIDAL IDOL",
+        songName: "ecstacy",
+        audio: ecstacyA
+    },
+
+    {
+        id: 7,
+        imageSrc: nokia,
+        singer: "Buta",
+        songName: "Nokia",
+        audio: nokiaA
+    },
+
+    {
+        id: 8,
         imageSrc: rinesanceImg,
         singer: "Rinesance",
         songName: "Dehu nMu",
@@ -92,174 +157,114 @@ export const songsData = [
     },
   
     {
-        id: 2,
+        id: 9,
         imageSrc: ekoImg,
         singer: "Eko",
         songName: "Rrethin e njejt",
         audio: ekoAudio
     },
-    {
-        id: 3,
-        imageSrc: skerdiImg,
-        singer: "Skerdi x Singi",
-        songName: "Cartel",
-        audio: skerdiAudio
-    },
-    {
-        id: 4,
-        imageSrc:  nuanceImg,
-        singer: "bletapertace ft EREZA",
-        songName: "nuance",
-        audio: nuanceAudio
-    },
-    {
-        id: 5,
-        imageSrc: kurMshehImg,
-        singer: "Buta",
-        songName: "Kur M'Sheh",
-        audio: kurMshehAudio
-    },
-    {
-        id: 6,
-        imageSrc: bvqhImg,
-        singer: "Rinesance",
-        songName: "BVQH",
-        audio: bvqhAudio
-    },
-    {
-        id: 7,
-        imageSrc: influence,
-        singer: "Eko",
-        songName: "Influence e keqe",
-        audio: influencea
-    },
 
-    {
-        id: 8,
-        imageSrc: vampirat,
-        singer: "Mal Maliqi",
-        songName: "Erdhen Vampirat",
-        audio: vampiratA
-    },
-    {
-        id: 9,
-        imageSrc: nokia,
-        singer: "Buta",
-        songName: "Nokia",
-        audio: nokiaA
-    },
     {
         id: 10,
-        imageSrc:esmeralda,
-        singer: "Amar",
-        songName: "Esmeralda",
-        audio: esmeraldaA
-    },
-
-
-    {
-        id: 11,
-        imageSrc: pare,
-        singer: "Ago ft Singi",
-        songName: "Pare",
-        audio: pareA
-    },
-    {
-        id: 12,
-        imageSrc: drake,
-        singer: "Drake",
-        songName: "Family Matters",
-        audio: drakeA
-    },
-    {
-        id: 13,
-        imageSrc: euphoria,
-        singer: "Kendrick Lamar",
-        songName: "Euphoria",
-        audio: euphoriaA
-    },
-    {
-        id: 14,
-        imageSrc: juice,
-        singer: "Juice Wrld",
-        songName: "All girls are the same",
-        audio: juiceA
-    },
-    {
-        id: 15,
-        imageSrc: fuck,
-        singer: "XXXTENTACION ft Trippie Red",
-        songName: "Fuck Love",
-        audio: fuckA
-    },
-    {
-        id: 16,
-        imageSrc: whore,
-        singer: "jvla",
-        songName: "such a whore",
-        audio: whoreA
-    },
-    {
-        id: 17,
-        imageSrc: ransom,
-        singer: "Lil Tecca",
-        songName: "Ransom",
-        audio: ransomA
-    },
-    {
-        id: 18,
         imageSrc: front,
         singer: "the front bottoms",
         songName: "twin size matters",
         audio: frontA
     },
     {
-        id: 19,
+        id: 11,
+        imageSrc: milk,
+        singer: "Rinesance",
+        songName: "Milk and Honey",
+        audio: milkA
+    },
+
+    {
+        id: 12,
         imageSrc: reflection,
         singer: "mac de marco",
         songName: "chamber of reflection",
         audio: reflectionA
     },
+
+ 
     {
-        id: 20,
-        imageSrc: ecstacy,
-        singer: "SUICIDAL IDOL",
-        songName: "ecstacy",
-        audio: ecstacyA
+        id: 13,
+        imageSrc: blondie,
+        singer: "Current Joys",
+        songName: "Blondie",
+        audio: blondieA
     },
+
     {
-        id: 21,
-        imageSrc: laufrey,
-        singer: "@laufrey",
-        songName: "From the start",
-        audio: laufreyA
+        id: 14,
+        imageSrc: better,
+        singer: "Penelope Scott",
+        songName: "Feel Better",
+        audio: betterA
     },
+
     {
-        id: 22,
+        id: 15,
+        imageSrc: breakit,
+        singer: "pinkpantheress",
+        songName: "break it off",
+        audio: breakitA
+    },
+
+    {
+        id: 16,
+        imageSrc: illusion,
+        singer: "vague003",
+        songName: "drowning",
+        audio: illusionA
+    },
+
+
+  
+    {
+        id: 17,
         imageSrc: cupid,
         singer: "Vietsub",
         songName: "Cupid's Chokehold",
         audio: cupidA
     },
+
+
+
     {
-        id: 23,
-        imageSrc: machine,
-        singer: "Mitski",
-        songName: "Washing Machine",
-        audio: machineA
+        id: 18,
+        imageSrc: poison,
+        singer: "Grouper",
+        songName: "Poison Tree",
+        audio: poisonA
     },
+
     {
-        id: 24,
-        imageSrc: scars,
-        singer: "Novulent",
-        songName: "scars",
-        audio: scarsA
+        id: 19,
+        imageSrc: alien,
+        singer: "Vundabar",
+        songName: "Alien Blues",
+        audio: alienA
     },
+
     {
-        id: 25,
-        imageSrc: rager,
-        singer: "Kid Cudi",
-        songName: "Mr Rager",
-        audio: ragerA
-    }
+        id: 20,
+        imageSrc: surprises,
+        singer: "Radiohead",
+        songName: "No surprises",
+        audio: surprisesA
+    },
+
+  
+
+    {
+        id: 21,
+        imageSrc: bvqhImg,
+        singer: "Rinesance",
+        songName: "BVQH",
+        audio: bvqhAudio
+    },
+
+
 ]
