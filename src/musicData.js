d24bb57cc5aea9ceafa97cd3befb4a3b9d968950
oltapllana <oltapllana@gmail.com>
2024-11-15 13:08:51 +0100

    first commit

--- src/musicData.js
+++ src/musicData.js
@@ -1,31 +1,10 @@
 
-import rinesanceImg from "./images/dehuNmu.png"
-import rinesanceAudio from "./audios/Rinesancë - Dehu n'mu.mp3";
 
 import ekoImg from "./images/rrethin.png"
 import ekoAudio from "./audios/EKO-RRETHIN-E-NJEJT-Prod-Pluto.mp3"
 
-
-
-import bvqhImg from "./images/rinesance.png"
-import bvqhAudio from "./audios/Rinesancë - BVQH (4K LOOPED VIDEO).mp3";
-
-import influence from "./images/eko.png"
-import influencea from "./audios/EKO-INFLUENCE-E-KEQE.mp3";
-
-
 import nokia from "./images/nokia.png"
 import nokiaA from "./audios/Buta-Nokia (The Lounge EP).mp3"
-
-
-
-
-
-
-
-
-import front from "./images/frontBottoms.png";
-import frontA from "./audios/twin size mattress - the front bottoms lyrics.mp3";
 
 import reflection from "./images/reflection.png";
 import reflectionA from "./audios/chamber of reflection mac demarco (lyrics).mp3";
@@ -33,37 +12,17 @@ import reflectionA from "./audios/chamber of reflection mac demarco (lyrics).mp3
 import ecstacy from "./images/ecstasy.png";
 import ecstacyA from "./audios/SUICIDAL-IDOL - ecstacy (slowed).mp3";
 
-import laufrey from "./images/laufrey.png";
-import laufreyA from "./audios/@laufey - From The Start (Lyrics).mp3";
-
-import cupid from "./images/cupid.png";
-import cupidA from "./audios/Vietsub Gym Class Heroes - Cupids Chokehold ft. Patrick Stump Nhạc Hot TikTok Lyrics Video.mp3";
-
 import machine from "./images/machine.png";
 import machineA from "./audios/Mitski - Washing Machine Heart (TikTok Remix) [Lyrics].mp3";
 
 
 
-import rager from "./images/rager.png";
-import ragerA from "./audios/Mr Rager - Kid Cudi (Lyrics).mp3";
-
 import abyss from "./images/abyss.png"
 import abyssA from "./audios/Abyss.mp3"
-
-import alien from "./images/alien.png"
-import alienA from "./audios/Vundabar - Alien Blues.mp3"
-
-import better from "./images/better.png"
-import betterA from "./audios/Feel Better - Penelope Scott  Lyrics.mp3"
-
-import blondie from "./images/blondie.png"
-import blondieA from "./audios/Blondie.mp3"
 
 import breakit from "./images/break.png"
 import breakitA from "./audios/pinkpantheress - break it off ( slowed  reverb ).mp3"
 
-import illusion from "./images/illusion.png"
-import illusionA from "./audios/none of it was real, it was an illusion... (vague003 - drowning).mp3"
 
 import milk from "./images/milk.png"
 import milkA from "./audios/Milk and Honey.mp3"
@@ -74,16 +33,240 @@ import pastlivesA from "./audios/sapientdream - Pastlives (lyrics).mp3"
 import poison from "./images/poison.png"
 import poisonA from "./audios/Grouper - Poison tree (Lyrics).mp3"
 
-
-import surprises from "./images/surprises.png"
-import surprisesA from "./audios/Radiohead - No Surprises (Lyrics).mp3"
-
 import takecare from "./images/takecare.png"
 import takecareA from "./audios/Take Care - Think Of Me Once In A While, Take Care.mp3"
+
+import bet500 from "./images/bet500.png"
+import bet500A from "./audios/MOSSI - BET 500.mp3"
+
+import carelessWhisper from "./images/carelessWhisper.png"
+import carelessWhisperA from "./audios/George Michael - Careless Whisper (Lyrics).mp3"
+
+import crazyStory from "./images/crazyStory.png"
+import crazyStoryA from "./audios/King Von Crazy Story (OTF) (WSHH Exclusive - Official Music Video).mp3"
+
+import ditelindja from "./images/ditelindja.png"
+import ditelindjaA from "./audios/YLL LIMANI - DITELINDJA (Official Music Video).mp3"
+
+import DiteSiKto from "./images/DiteSiKto.png"
+import DiteSiKtoA from "./audios/Rinesancë - N'ditë si kto (prod.wydsonni).mp3"
+
+import faleminderit from "./images/faleminderit.png"
+import faleminderitA from "./audios/Buta-Faleminderit (The Lounge EP).mp3"
+
+import fedex from "./images/fedex.png"
+import fedexA from "./audios/EKO - FEDEX (Prod. Pluto).mp3"
+
+import hoodLife from "./images/hoodLife.png"
+import hoodLifeA from "./audios/Vinz ft Stealth - Hood Life 3.mp3"
+
+import ikuBuzza from "./images/ikuBuzza.png"
+import ikuBuzzaA from "./audios/Cllevio Serbiano - Iku buzza.mp3"
+
+import kg from "./images/kg.png"
+import kgA from "./audios/SINGI feat. MC KRESHA - KG (Official Video).mp3"
+
+import kur from "./images/kur.png"
+import kurA from "./audios/AGO - Kur.mp3"
+
+import likeThat from "./images/likeThat.png"
+import likeThatA from "./audios/Future, Metro Boomin, Kendrick Lamar - Like That (Lyrics).mp3"
+
+import mister from "./images/mister.png"
+import misterA from "./audios/MOSSI - MISTER.mp3"
+
+import myEyes from "./images/myEyes.png"
+import myEyesA from "./audios/Travis Scott - MY EYES  (Lyrics).mp3"
+
+import navai from "./images/navai.png"
+import navaiA from "./audios/Navai, MONA - Есенин.mp3"
+
+import newSwag from "./images/newSwag.png"
+import newSwagA from "./audios/EKO - NEW SWAG (Visualizer).mp3"
+
+import pare from "./images/pare.png"
+import pareA from "./audios/Ago x Singi - Pare (remaster).mp3"
+
+import pitbull from "./images/pitbull.png"
+import pitbullA from "./audios/Singi X Buta - Pitbull.mp3"
+
+import remedi from "./images/remedi.png"
+import remediA from "./audios/EKO - REMEDI (Visualizer).mp3"
+
+import runaway from "./images/runaway.png"
+import runawayA from "./audios/Kanye West - Runaway (Lyrics) ft. Pusha T.mp3"
+
+import sickMode from "./images/sickMode.png"
+import sickModeA from "./audios/Travis Scott - SICKO MODE (Lyrics) ft. Drake.mp3"
+
+import skomUnFaj from "./images/skomUnFaj.png"
+import skomUnFajA from "./audios/EKO - SKOM UN FAJ (Prod. Pluto).mp3"
+
+import star from "./images/star.png"
+import starA from "./audios/Lil Peep - Star Shopping (Lyrics).mp3"
+
+import thinkingAboutYou from "./images/thinkingAboutYou.png"
+import thinkingAboutYouA from "./audios/Frank Ocean - Thinkin bout you (Lyrics).mp3"
+
+import thirreNjerin from "./images/thirre njerin.png"
+import thirreNjerinA from "./audios/Çelik Lipa x Lumi B x Singi - THIRRE NJERIN.mp3"
+
+import typeShit from "./images/typeShit.png"
+import typeShitA from "./audios/Future, Metro Boomin - Type Shit (Lyrics).mp3"
+
+import vullnetar from "./images/vullnetar.png"
+import vullnetarA from "./audios/ARTI - VULLNETAR.mp3"
+
+import yessirski from "./images/yessirski.png"
+import yessirskiA from "./audios/Ago x Buta - Yessirski.mp3"
+
+import zemerthys from "./images/zemerthys.png"
+import zemerthysA from "./audios/EKO - ZEMERTHYS (Visualizer).mp3"
+
+import rrethin from "./images/rrethin.png"
+import rrethinA from "./audios/EKO-RRETHIN-E-NJEJT-Prod-Pluto.mp3"
+
 
 
 
 export const songsData = [
+    {
+        id: 21,
+        imageSrc: remedi,
+        singer: "Eko",
+        songName: "Remedi",
+        audio: remediA
+    },
+    {
+        id: 6,
+        imageSrc: DiteSiKto,
+        singer: "Rinesance",
+        songName: "n'dite si kto",
+        audio: DiteSiKtoA
+    },
+    {
+        id: 11,
+        imageSrc: ikuBuzza,
+        singer: "Cllevio",
+        songName: "Iku Buzza",
+        audio: ikuBuzzaA
+    },
+    {
+        id: 21,
+        imageSrc: newSwag,
+        singer: "Eko",
+        songName: "New Swag",
+        audio: newSwagA
+    },
+    {
+        id: 23,
+        imageSrc: pare,
+        singer: "Ago ft Singi (remaster)",
+        songName: "Pare",
+        audio: pareA
+    },
+    {
+        id: 21,
+        imageSrc: pitbull,
+        singer: "Singi ft Buta",
+        songName: "Pitbull",
+        audio: pitbullA
+    },
+    {
+        id: 20,
+        imageSrc: navai,
+        singer: "Navai ft MONA",
+        songName: "Есенин",
+        audio: navaiA
+    },
+    
+    {
+        id: 21,
+        imageSrc: typeShit,
+        singer: "Future ft Metro Boomin",
+        songName: "Type Shit",
+        audio: typeShitA
+    },
+    {
+        id: 21,
+        imageSrc: zemerthys,
+        singer: "Eko",
+        songName: "Zemerthys",
+        audio: zemerthysA
+    },
+    {
+        id: 21,
+        imageSrc: thirreNjerin,
+        singer: "LumiB ft Singi",
+        songName: "Thirre Njerin",
+        audio: thirreNjerinA
+    },
+    {
+        id: 21,
+        imageSrc: star,
+        singer: "Lil Peep",
+        songName: "Star shopping",
+        audio: starA
+    },
+    {
+        id: 21,
+        imageSrc: skomUnFaj,
+        singer: "Eko",
+        songName: "Skom un faj",
+        audio: skomUnFajA
+    },
+    {
+        id: 1,
+        imageSrc: bet500,
+        singer: "Mossi",
+        songName: "Bet 500",
+        audio: bet500A
+    },
+    {
+        id: 3,
+        imageSrc: carelessWhisper,
+        singer: "George Michael",
+        songName: "Careless Whisper",
+        audio: carelessWhisperA
+    },
+    {
+        id: 5,
+        imageSrc: ditelindja,
+        singer: "Yll Limani",
+        songName: "Ditelindja",
+        audio: ditelindjaA
+    },
+    {
+        id: 7,
+        imageSrc: ecstacy,
+        singer: "SUICIDAL IDOL",
+        songName: "ecstasy",
+        audio: ecstacyA
+    },
+    {
+        id: 9,
+        imageSrc: fedex,
+        singer: "Eko",
+        songName: "Fedex",
+        audio: fedexA
+    },
+    {
+        id: 13,
+        imageSrc: kg,
+        singer: "Singi ft McKresha",
+        songName: "KG",
+        audio: kgA
+    },
+    {
+        id: 15,
+        imageSrc: likeThat,
+        singer: "Future ft Kendrick Lamar ft Metro Boomin",
+        songName: "Like that",
+        audio: likeThatA
+    },
+
+
+
 
     {
         id: 0,
@@ -92,179 +275,206 @@ export const songsData = [
         songName: "Abyss",
         audio: abyssA
     },
-
     {
-        id: 1,
-        imageSrc: takecare,
-        singer: "Take Care",
-        songName: "Think of me once in a while",
-        audio: takecareA
+        id: 18,
+        imageSrc: mister,
+        singer: "Mossi",
+        songName: "Mister",
+        audio: misterA
     },
+    {
+        id: 4,
+        imageSrc: crazyStory,
+        singer: "King Von",
+        songName: "Crazy Story",
+        audio: crazyStoryA
+    },
+ 
+  
 
     {
         id: 2,
-        imageSrc: laufrey,
-        singer: "@laufrey",
-        songName: "From the start",
-        audio: laufreyA
-    },
-
-    {
-        id: 3,
-        imageSrc: pastlives,
-        singer: "sapientdream",
-        songName: "Pastlives",
-        audio: pastlivesA
-    },
-
-    {
-        id: 4,
-        imageSrc: influence,
-        singer: "Eko",
-        songName: "Influence e keqe",
-        audio: influencea
-    },
-    {
-        id: 5,
-        imageSrc: machine,
-        singer: "Mitski",
-        songName: "Washing Machine",
-        audio: machineA
-    },
-
-    {
-        id: 6,
-        imageSrc: ecstacy,
-        singer: "SUICIDAL IDOL",
-        songName: "ecstacy",
-        audio: ecstacyA
-    },
-
-    {
-        id: 7,
-        imageSrc: nokia,
-        singer: "Buta",
-        songName: "Nokia",
-        audio: nokiaA
-    },
-
-    {
-        id: 8,
-        imageSrc: rinesanceImg,
-        singer: "Rinesance",
-        songName: "Dehu nMu",
-        audio: rinesanceAudio
-    },
-  
-    {
-        id: 9,
-        imageSrc: ekoImg,
-        singer: "Eko",
-        songName: "Rrethin e njejt",
-        audio: ekoAudio
-    },
-
-    {
-        id: 10,
-        imageSrc: front,
-        singer: "the front bottoms",
-        songName: "twin size matters",
-        audio: frontA
-    },
-    {
-        id: 11,
-        imageSrc: milk,
-        singer: "Rinesance",
-        songName: "Milk and Honey",
-        audio: milkA
-    },
-
-    {
-        id: 12,
-        imageSrc: reflection,
-        singer: "mac de marco",
-        songName: "chamber of reflection",
-        audio: reflectionA
-    },
-
- 
-    {
-        id: 13,
-        imageSrc: blondie,
-        singer: "Current Joys",
-        songName: "Blondie",
-        audio: blondieA
-    },
-
-    {
-        id: 14,
-        imageSrc: better,
-        singer: "Penelope Scott",
-        songName: "Feel Better",
-        audio: betterA
-    },
-
-    {
-        id: 15,
         imageSrc: breakit,
-        singer: "pinkpantheress",
-        songName: "break it off",
+        singer: "Pinkpantheress",
+        songName: "Break it off",
         audio: breakitA
     },
 
+  
+
+   
+
+   
+
+   
+    {
+        id: 8,
+        imageSrc: faleminderit,
+        singer: "Buta",
+        songName: "Faleminderit",
+        audio: faleminderitA
+    },
+  
+  
+    {
+        id: 10,
+        imageSrc: hoodLife,
+        singer: "Vinz ft Stealth",
+        songName: "Hood Life 3",
+        audio: hoodLifeA
+    },
+ 
+
+   
+ 
+   
+
+    {
+        id: 14,
+        imageSrc: kur,
+        singer: "Ago",
+        songName: "Kur",
+        audio: kurA
+    },
+
+    
+
     {
         id: 16,
-        imageSrc: illusion,
-        singer: "vague003",
-        songName: "drowning",
-        audio: illusionA
+        imageSrc: machine,
+        singer: "Mitski",
+        songName: "Washing machine",
+        audio: machineA
     },
 
 
   
     {
         id: 17,
-        imageSrc: cupid,
-        singer: "Vietsub",
-        songName: "Cupid's Chokehold",
-        audio: cupidA
+        imageSrc: milk,
+        singer: "Rinesance",
+        songName: "Milk and honey",
+        audio: milkA
     },
 
 
 
+
+
     {
-        id: 18,
+        id: 19,
+        imageSrc: myEyes,
+        singer: "Travis Scott",
+        songName: "My eyes",
+        audio: myEyesA
+    },
+
+   
+
+  
+
+   
+    {
+        id: 22,
+        imageSrc: nokia,
+        singer: "Buta",
+        songName: "Nokia",
+        audio: nokiaA
+    },
+    {
+        id: 21,
+        imageSrc: vullnetar,
+        singer: "Arti",
+        songName: "Vullnetar",
+        audio: vullnetarA
+    },
+
+    {
+        id: 21,
+        imageSrc: yessirski,
+        singer: "Ago ft Buta",
+        songName: "Yessirski",
+        audio: yessirskiA
+    },
+
+
+   
+
+    {
+        id: 21,
+        imageSrc: pastlives,
+        singer: "sapientdream",
+        songName: "PastLives",
+        audio: pastlivesA
+    },
+
+    
+    {
+        id: 21,
         imageSrc: poison,
         singer: "Grouper",
-        songName: "Poison Tree",
+        songName: "Poison tree",
         audio: poisonA
     },
 
     {
-        id: 19,
-        imageSrc: alien,
-        singer: "Vundabar",
-        songName: "Alien Blues",
-        audio: alienA
+        id: 21,
+        imageSrc: reflection,
+        singer: "Mac de marco",
+        songName: "Chamber of reflection",
+        audio: reflectionA
+    },
+
+
+
+    {
+        id: 21,
+        imageSrc: rrethin,
+        singer: "Eko",
+        songName: "Rrethin e njejt",
+        audio: rrethinA
     },
 
     {
-        id: 20,
-        imageSrc: surprises,
-        singer: "Radiohead",
-        songName: "No surprises",
-        audio: surprisesA
+        id: 21,
+        imageSrc: runaway,
+        singer: "Kanye West",
+        songName: "Runaway",
+        audio: runawayA
     },
+
+    {
+        id: 21,
+        imageSrc: sickMode,
+        singer: "Travis Scott",
+        songName: "Sicko Mode",
+        audio: sickModeA
+    },
+
+  
 
   
 
     {
         id: 21,
-        imageSrc: bvqhImg,
-        singer: "Rinesance",
-        songName: "BVQH",
-        audio: bvqhAudio
+        imageSrc: takecare,
+        singer: "Take care",
+        songName: "Think of me once in a while",
+        audio: takecareA
     },
+
+    {
+        id: 21,
+        imageSrc: thinkingAboutYou,
+        singer: "Frank Ocean",
+        songName: "Thinking about you",
+        audio: thinkingAboutYouA
+    },
+
+  
+
+
+ 
 
 
 ]
